b9d9469848067c548c95f776683e8d30df5149ad
gerlynm <martingerlyn@gmail.com>
2023-10-26 22:09:33 +0530

    changes made in backend

--- modules/backend/backend.tf
+++ modules/backend/backend.tf
@@ -1,12 +1,9 @@
-/*
 terraform {
   backend "s3" {
-    bucket         =  "jerriitesters3bucket.in"
-    key            = "test/terraform.tfstate"
+    bucket         = "jerriitesters3bucket.in"
+    key            = "statefile/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
-    dynamodb_table = 
+    dynamodb_table = "test"
   }
 }
-
-*/
